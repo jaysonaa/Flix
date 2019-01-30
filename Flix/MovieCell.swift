e887ae6c9810285c7352a250cf7de6e8f5d39494
Jay-son Alburg <Jay-son@jay-sons-macbook-pro.local>
2019-01-29 21:46:02 -0500

    flix final

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Jay-son Alburg on 1/29/19.
+//  Copyright © 2019 Jay'son Alburg. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
